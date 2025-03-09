fde2defe6fefe0419bacd82f4d76136ccee7d891
Ayankhan086 <kmayankhan1@gmail.com>
2025-03-09 15:01:55 +0500

    Utills code creation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/dbconnect.js";
+import app from "./app.js"
+
 
 dotenv.config(
     {
@@ -11,4 +13,15 @@ dotenv.config(
 );
 
 
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port ${process.env.PORT || 8000}`);
+    })
+    app.on('error', (error) => {
+        console.error(`Error: ${error.message
+            }`);
+    })
+}).catch((error) => {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+});
