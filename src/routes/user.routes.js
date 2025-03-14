6b45c304896708f83ba9a8341c1a0ff668dba0dd
Ayankhan086 <kmayankhan1@gmail.com>
2025-03-14 22:00:44 +0500

    ``` Add functionality for user profile picture and cover image updates ```

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,7 +36,7 @@ router.route("/changed_password").post(verifyjwt, changeCurrentPassword)
 router.route("/current-user").get(verifyjwt, getCurrentUser)
 router.route("/update-account-details").patch(verifyjwt, updateAccountDetails)
 router.route('/avatar').patch(verifyjwt, upload.single("avatar"), updateUserAvatar)
-router.route("/coverimage").patch(verifyjwt, upload.single("/coverimage"), updateUserCoverImage)
+router.route("/coverimage").patch(verifyjwt, upload.single("coverimage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyjwt, getUserChannelProfile)
 router.route("/watchHistory").get(verifyjwt, getWatcHistory)
